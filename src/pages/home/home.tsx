b2f894bc552d33643a75d5564e3abb936be0eeae
Kushal Tak <ktak1996@gmail.com>
2023-09-04 23:58:34 +0530

    added a welcome msg

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-    return ( 
-        <h1>This is Home</h1>
-     );
-}
- 
+  return (
+    <div className="centered-container">
+      <div className="card">
+        <h3 className="title">
+          Keep Calm! Product development is in progress..
+        </h3>
+        <section className="content">
+          <b>Kushal Tak,</b> Full stack developer with sound concepts and
+          demonstrating experience in MERN Stack, MEAN Stack, Python, Django,
+          Data Structures and Algorithms. Strong engineering professional with a
+          Bachelor of Engineering focused in Information Technology from Shri G
+          S Institute of Technology & Science.
+        </section>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
